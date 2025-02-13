6cc73110296e3f1dbffbd1583fb377869ac22c75
Garazu <garazu@gmail.com>
2025-02-13 17:37:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RPGdaVida());
+}
+
+class RPGdaVida extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RPG da Vida',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('RPG da Vida')),
+      body: Center(child: Text('Bem-vindo ao RPG da Vida!')),
+    );
+  }
+}
 
